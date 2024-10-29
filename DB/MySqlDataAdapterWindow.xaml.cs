dacf8b552b428d2dc699ca2ab805823186fd7cd3
Song Young Ohk <43607595+YOSong1@users.noreply.github.com>
2024-10-30 08:24:33 +0900

    Add files via upload

---
+++ DB/MySqlDataAdapterWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows;
+
+
+namespace WpfDB
+{
+    public partial class MySqlDataAdapterWindow : Window
+    {
+        public MySqlDataAdapterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // MySQL 연결 문자열 설정
+                string myConnection = "Server=localhost;Database=wpf;Port=3306;User=root;Password=1234";
+
+                // MySqlConnection 클래스를 사용하여 연결 생성
+                MySqlConnection connection = new MySqlConnection(myConnection);
+
+                // MySqlDataAdapter를 사용하여 데이터를 가져올 쿼리 설정
+                MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
+                myDataAdapter.SelectCommand = new MySqlCommand("select * from users;", connection);
+
+                // MySqlCommandBuilder를 사용하여 데이터 업데이트 작업을 자동으로 생성
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(myDataAdapter);
+
+                // 데이터베이스 연결 열기
+                connection.Open();
+
+                // 데이터를 저장할 DataSet 생성
+                DataSet ds = new DataSet();
+
+                // 데이터베이스에서 데이터를 가져와 ds에 저장
+                myDataAdapter.Fill(ds, "users");
+
+                // DataGrid에 데이터 표시
+                userDataGrid.ItemsSource = ds.Tables["users"].DefaultView;
+
+                MessageBox.Show("데이터 가져오기 완료!");
+
+                // 데이터베이스 연결 닫기
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ DB/MySqlDataReaderWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDB
+{
+    /// <summary>
+    /// MySqlDataReaderWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MySqlDataReaderWindow : Window
+    {
+        public ObservableCollection<User> Users { get; set; }
+
+        public MySqlDataReaderWindow()
+        {
+            InitializeComponent();
+            Users = new ObservableCollection<User>();
+            userDataGrid.ItemsSource = Users;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string myConnection = "Server=localhost;Database=wpf;Port=3306;User=root;Password=1234";
+
+                using (MySqlConnection connection = new MySqlConnection(myConnection))
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand("SELECT * FROM users;", connection);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        Users.Clear();
+                        while (reader.Read())
+                        {
+                            var user = new User
+                            {
+                                ID = reader.GetInt32("ID"),
+                                Name = reader.GetString("Name"),
+                                Password = reader.GetString("Password")
+                            };
+                            Users.Add(user);
+                        }
+                    }
+                }
+
+                MessageBox.Show("데이터 가져오기 완료!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
